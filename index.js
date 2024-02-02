e1e750727a390c4ccf0b433897cd43e29ce42cf8
Julian Kisner <julikisner@gmail.com>
2024-02-02 16:22:56 -0300

    change

--- index.js
+++ index.js
@@ -56,9 +56,7 @@ io.on("connection", async (socket) => {
   socket.on("session", ({ session, userInfo }) => {
     console.log("Received session information:", session);
     socket.userInfo = userInfo;
-    socket.broadcast.emit("receive_message", data); 
-    
-
+    socket.broadcast.emit("receive_message", userInfo); 
   });
   console.log("Usuario conectado:", socket.userInfo);
 
